05c7972e40b207f0a4ebad936478521d1def1659
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2025-03-14 16:38:41 -0700

    whychoose api fetches, need to style

--- cls-frontend/src/sections/whyChooseUs.jsx
+++ cls-frontend/src/sections/whyChooseUs.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react';
 import './whyChooseUs.css'
 import { fetchWhyChooseUs } from '../services/api/whyChooseUs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faUser, faClipboardList, faHandshake } from '@fortawesome/free-solid-svg-icons';
 
 const WhyChooseUs = () => {
 
@@ -14,10 +16,19 @@ const WhyChooseUs = () => {
         loadWhyChooseUsData();
     }, []);    
 
+    const icons = [faLocationDot, faUser, faClipboardList, faHandshake]; 
+
     return (
         <section className='whyChooseUs__container'>
             <h2 className="whyChooseUs__heading">Why Choose Us?</h2>
             <h4 className="whyChooseUs__subheading">{whyChooseUsData?.subheading}</h4>
+                {whyChooseUsData?.reasons?.slice(0,4).map((reason, index) => (
+                    <div key={index} className="whyChooseUs__card">
+                        <FontAwesomeIcon icon={icons[index]} className='card__icon' />
+                        <h4 className="card__title">{reason.title}</h4>
+                        <h5 className="card__descr">{reason.description}</h5>
+                    </div>
+                ))}
         </section>
     );
 }
--- cls-frontend/src/services/api/whyChooseUs.js
+++ cls-frontend/src/services/api/whyChooseUs.js
@@ -2,17 +2,17 @@ import { supabase } from "../supabaseClient";
 
 export async function fetchWhyChooseUs() {
   const { data, error } = await supabase
-    .from("whychooseus__section") // landscapeh__section table
+    .from("whychooseus__section") 
     .select('subheading, reasons')
-    .limit(1) //retrieves only 1 row
-    .maybeSingle();
+    .limit(1)
 
   console.log("Fetched whychooseus data:", data);
   console.log("Error:", error);
+
 
   if (error) {
     console.error("Error fetching whyChooseUs section:", error);
     return null;
   }
-  return data;
+  return data.length > 0 ? data[0] : null;
 }
