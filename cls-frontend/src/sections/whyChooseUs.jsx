a464df599194dc24622baa1c65adc9f91cab125c
James Daniel Cadavona <63523469+Jameboyyy@users.noreply.github.com>
2025-03-15 13:40:18 -0700

    wcu styling

--- cls-frontend/src/sections/whyChooseUs.jsx
+++ cls-frontend/src/sections/whyChooseUs.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect} from 'react';
 import './whyChooseUs.css'
 import { fetchWhyChooseUs } from '../services/api/whyChooseUs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot, faUser, faClipboardList, faHandshake } from '@fortawesome/free-solid-svg-icons';
-
+import { faLocationDot, faUser, faClipboardList, faHandshakeAngle } from '@fortawesome/free-solid-svg-icons';
+//niggs
 const WhyChooseUs = () => {
 
     const [whyChooseUsData, setWhyChooseUsData] = useState(null);   
 
+    
     useEffect(() => {
         async function loadWhyChooseUsData() {
             const data = await fetchWhyChooseUs();
@@ -16,19 +17,30 @@ const WhyChooseUs = () => {
         loadWhyChooseUsData();
     }, []);    
 
-    const icons = [faLocationDot, faUser, faClipboardList, faHandshake]; 
+    const icons = [faLocationDot, faUser, faClipboardList, faHandshakeAngle]; 
 
     return (
         <section className='whyChooseUs__container'>
-            <h2 className="whyChooseUs__heading">Why Choose Us?</h2>
-            <h4 className="whyChooseUs__subheading">{whyChooseUsData?.subheading}</h4>
-                {whyChooseUsData?.reasons?.slice(0,4).map((reason, index) => (
+            <div className="whyChooseUs__heading--wrapper">
+                <h2 className="whyChooseUs__heading">Why Choose Us?</h2>
+                <h3 className="whyChooseUs__subheading">{whyChooseUsData?.subheading}</h3>
+            </div>
+            <div className="wcu__card--wrapper">
+                {whyChooseUsData?.reasons?.slice(0, 4).map((reason, index) => (
                     <div key={index} className="whyChooseUs__card">
-                        <FontAwesomeIcon icon={icons[index]} className='card__icon' />
-                        <h4 className="card__title">{reason.title}</h4>
-                        <h5 className="card__descr">{reason.description}</h5>
+                        <div className="icon__bg">
+                            <FontAwesomeIcon 
+                                icon={icons[index]} 
+                                className={`card__icon ${icons[index] === faHandshakeAngle ? "handshake__icon" : ""}`} 
+                            />
+                        </div>
+                        <div className="wcu__text--wrapper">
+                            <h4 className="card__title">{reason.title}</h4>
+                            <h5 className="card__descr">{reason.description}</h5>
+                        </div>
                     </div>
                 ))}
+            </div>  
         </section>
     );
 }
