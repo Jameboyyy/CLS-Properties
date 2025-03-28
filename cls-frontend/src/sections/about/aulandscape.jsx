db56b0d31c8e624bf96ad22e3ee062416dc2586c
Phuoc Nguyen <phuocnguyen102800@gmail.com>
2025-03-28 09:17:25 -0700

    Made the images fit the box. Also added shadows. just need to change the colors and give it more opacity

--- cls-frontend/src/sections/about/aulandscape.jsx
+++ cls-frontend/src/sections/about/aulandscape.jsx
@@ -18,19 +18,19 @@ const Aulandscape = () => {
         <section className='aulandscape__container'>
             <h2 className="aulandscape__heading">"Where Dreams Come Home"</h2>
             <div className="au__info--container">
-                <div className="au__info--card">
+                <div className="au__info--card--tl">
                     <div className="au__info--text">
                         <h1 className="au__info--heading">About Us</h1>
                         <h3 className="au__info--desc">{auData?.description}</h3>
                     </div>
                 </div>
-                <div className="au__info--card">
+                <div className="au__info--card--tr">
                     <img src={auData?.team__img__url} alt="" className="au__info--img" />
                 </div>
-                <div className="au__info--card">
+                <div className="au__info--card--bl">
                     <img src={auData?.property__img__url} alt="" className="au__info--img" />
                 </div>
-                <div className="au__info--card">
+                <div className="au__info--card--br">
                     <div className="au__info--text">
                         <h1 className="au__info--heading">Mission</h1>
                         <h3 className="au__info--desc">{auData?.mission}</h3>
